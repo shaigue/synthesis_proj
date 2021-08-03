d63ccd418425df1341201d840e1754252e03467e
Shai Guendelman <shaigue@gmail.com>
2021-08-03 15:59:44 +0300

    added todo

---
+++ tests/basic_integer_tests/test1.py
@@ -0,0 +1,8 @@
+# TODO: each test will be composed of the following parts - 
+# 1. program with a single loop, .py file
+# 2. positive program states, .json file
+# 3. negative program states, .json file
+# 4. pre-condition
+# 5. post-condition
+# 6. a loop invariant / or if there is no loop invariant (the property do not hold)
+# TODO: a utility for loading those
