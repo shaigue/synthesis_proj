4222b426a3a2dffc11c484b0c744dd108a415056
Shai Guendelman <shaigue@gmail.com>
2021-08-05 12:54:17 +0300

    added negative states generation (randomly)

--- tests/collect_states.py
+++ tests/collect_states.py
@@ -1,8 +1,7 @@
-# TODO - collect also negative examples
-
 import ast
 from _ast import While, Module
 from typing import Any
+import random
 
 import config
 
@@ -31,7 +30,7 @@ class CollectLoopStatesTransformer(ast.NodeTransformer):
         )
 
 
-def collect_positive_states(program_ast: ast.Module) -> list[dict]:
+def collect_positive_states(program_ast: ast.Module) -> list[dict[str, int]]:
     transformed_ast = CollectLoopStatesTransformer().visit(program_ast)
     transformed_ast = ast.fix_missing_locations(transformed_ast)
     transformed_code = compile(transformed_ast, filename='<string>', mode='exec')
@@ -39,14 +38,35 @@ def collect_positive_states(program_ast: ast.Module) -> list[dict]:
     return locals()['_states']
 
 
-def example():
-    strip_src = """
-    x = 1
-    n = 10
-    while n > 0:
-        n = n - 1
-    """
-    print(collect_positive_states(ast.parse(strip_src)))
+def collect_negative_states_randomly(positive_states: list[dict[str, int]], n_states: int = 5) -> list[dict[str, int]]:
+    # TODO: after that the generation of the predicate is complete, we can rank randomly generated states by how
+    #   much simple the term gets when adding them, or in some other way.
+    #   the idea is that the negative samples are also something that can be "synthesized", until the property is
+    #   proved. if this doesn't work just manually add negative states
+    # TODO: allow types other then integers
+    variables = set()
+    for state in positive_states:
+        variables.update(state.keys())
+    negative_states = []
+    while len(negative_states) < n_states:
+        state = {}
+        for v in variables:
+            state[v] = random.randint(-100, 100)
+        if state not in positive_states and state not in negative_states:
+            negative_states.append(state)
+    return negative_states
+
+
+EXAMPLE_SRC = """
+x = 1
+n = 10
+while n > 0:
+    n = n - 1
+"""
+
+
+def positive_example():
+    print(collect_positive_states(ast.parse(EXAMPLE_SRC)))
 
 
 def try_on_all_tests():
@@ -54,9 +74,10 @@ def try_on_all_tests():
     for program_path in tests_dir.rglob('program.py'):
         program_ast = ast.parse(program_path.read_text())
         positive_states = collect_positive_states(program_ast)
-        print(f'{program_path}\n{positive_states}')
+        negative_states = collect_negative_states_randomly(positive_states)
+        print(f'{program_path}\nPOSITIVE={positive_states}\nNEGATIVE={negative_states}')
 
 
 if __name__ == '__main__':
-    example()
+    positive_example()
     try_on_all_tests()
