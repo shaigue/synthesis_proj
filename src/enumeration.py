f169043d036a9642e99123721576532b6b71b116
abialystok <asaf.b@campus.technion.ac.il>
2021-08-20 18:37:19 +0300

    Add "and chaining"

--- src/enumeration.py
+++ src/enumeration.py
@@ -1,6 +1,7 @@
 from typing import List, Dict
 from parsing.earley.parser import Grammar
 from datetime import datetime
+from itertools import combinations
 import config
 
 from pathlib import Path
@@ -57,16 +58,18 @@ def predicates_from_rule_generator(rule: List[str], predicates, depth):
         yield from predicates_from_rule_generator(new_rule, predicates, depth)
 
 
-def enumerate_predicates(grammar: Grammar, inputs: List[Dict]):
+def enumerate_predicates(grammar: Grammar, positive_inputs: List[Dict], negative_inputs: List[Dict]):
     """
     enumerate over given grammar, with observational equivalence (Generator)
     programs are kept in the predictes dictionary, of type Dict[int, Dict[str, Dict[tuple, str]]]
     key of uppermost dict is depth, key of next dict is lhs of the rule, key of next dict is tuple of results from
     the evaluation of the predicate on different input (length of tuple is amount of inputs).
     :param grammar: the grammar
-    :param inputs: inputs on which to base the observational equivalence
+    :param positive_inputs: inputs on which the LI should be evaluated to True
+    :param negative_inputs: inputs on which the LI should be evaluated to False
     :return: yields programs, from depth 0 until MAX_DEPTH
     """
+    inputs = positive_inputs + negative_inputs
     # Deal with depth = 0
     predicates = {0: {}}
     for lhs, rhs_list in grammar.rules.items():
@@ -86,16 +89,27 @@ def enumerate_predicates(grammar: Grammar, inputs: List[Dict]):
             for rhs in rhs_list:
                 for new_predicate in predicates_from_rule_generator(rhs.rhs, predicates, i):
                     evaluation = evaluate_predicate(new_predicate, inputs)
-                    predicates[i][lhs][evaluation] = new_predicate
+                    if evaluation not in predicates[i][lhs]:
+                        predicates[i][lhs][evaluation] = new_predicate
 
         for predicate in predicates[i][grammar.start_symbol].values():
             yield predicate
 
+        positive_predicates = [pred for evaluation, pred in predicates[i][grammar.start_symbol].items()
+                               if len(list(filter(lambda x: not x, evaluation[:len(positive_inputs)]))) == 0]
+
+        for d in range(2, 4):
+            positive_groups = list(combinations(positive_predicates, d))
+            for g in positive_groups:
+                ret = (d - 1) * "( " + f"{g[0]}"
+                for p in g[1:]:
+                    ret += f" and {p} )"
+                yield ret
+
 
 def find_loop_invariant(grammar_string: str, positive_inputs: [List[Dict]], negative_inputs: List[Dict]) -> str:
     grammar = Grammar.from_string(grammar_string)
-    for pred in enumerate_predicates(grammar, positive_inputs + negative_inputs):
-        print(pred)
+    for pred in enumerate_predicates(grammar, positive_inputs, negative_inputs):
         if (len(list(filter(lambda x: not x, evaluate_predicate(pred, positive_inputs)))) == 0 and
                 len(list(filter(lambda x: x, evaluate_predicate(pred, negative_inputs)))) == 0):
             return pred
