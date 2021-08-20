bf5a034a730bb4f54e369a0aecacb77526bcfcae
abialystok <asaf.b@campus.technion.ac.il>
2021-08-20 17:18:11 +0300

    Added wrapper function that returns the first valid LI

--- src/enumeration.py
+++ src/enumeration.py
@@ -1,7 +1,10 @@
-
 from typing import List, Dict
 from parsing.earley.parser import Grammar
 from datetime import datetime
+import config
+
+from pathlib import Path
+from test_utils.positive_state_extractor import collect_positive_states_from_file
 
 MAX_DEPTH = 4
 
@@ -54,19 +57,16 @@ def predicates_from_rule_generator(rule: List[str], predicates, depth):
         yield from predicates_from_rule_generator(new_rule, predicates, depth)
 
 
-def enumerate_predicates(grammar_str: str, base_rule: str, inputs: List[Dict]):
+def enumerate_predicates(grammar: Grammar, inputs: List[Dict]):
     """
     enumerate over given grammar, with observational equivalence (Generator)
     programs are kept in the predictes dictionary, of type Dict[int, Dict[str, Dict[tuple, str]]]
     key of uppermost dict is depth, key of next dict is lhs of the rule, key of next dict is tuple of results from
     the evaluation of the predicate on different input (length of tuple is amount of inputs).
-    :param grammar_str: the grammar
-    :param base_rule: LHS of the base rule of the grammar. Only predicates that are derived from this rule are complete.
+    :param grammar: the grammar
     :param inputs: inputs on which to base the observational equivalence
     :return: yields programs, from depth 0 until MAX_DEPTH
     """
-    grammar = Grammar.from_string(grammar_str)
-
     # Deal with depth = 0
     predicates = {0: {}}
     for lhs, rhs_list in grammar.rules.items():
@@ -76,10 +76,10 @@ def enumerate_predicates(grammar_str: str, base_rule: str, inputs: List[Dict]):
                 evaluation = evaluate_predicate(pred, inputs)
                 if evaluation not in predicates[0].setdefault(lhs, {}):
                     predicates[0][lhs][evaluation] = pred
-                    if lhs == base_rule:
+                    if lhs == grammar.start_symbol:
                         yield pred
 
-    for i in range(1, MAX_DEPTH+1):
+    for i in range(1, MAX_DEPTH + 1):
         predicates[i] = {}
         for lhs, rhs_list in grammar.rules.items():
             predicates[i][lhs] = {}
@@ -88,8 +88,17 @@ def enumerate_predicates(grammar_str: str, base_rule: str, inputs: List[Dict]):
                     evaluation = evaluate_predicate(new_predicate, inputs)
                     predicates[i][lhs][evaluation] = new_predicate
 
-        for predicate in predicates[i][base_rule].values():
+        for predicate in predicates[i][grammar.start_symbol].values():
             yield predicate
+
+
+def find_loop_invariant(grammar_string: str, positive_inputs: [List[Dict]], negative_inputs: List[Dict]) -> str:
+    grammar = Grammar.from_string(grammar_string)
+    for pred in enumerate_predicates(grammar, positive_inputs + negative_inputs):
+        print(pred)
+        if (len(list(filter(lambda x: not x, evaluate_predicate(pred, positive_inputs)))) == 0 and
+                len(list(filter(lambda x: x, evaluate_predicate(pred, negative_inputs)))) == 0):
+            return pred
 
 
 if __name__ == "__main__":
@@ -103,72 +112,43 @@ if __name__ == "__main__":
     LOP -> and | or
     AOP -> + | - | * | // | %
     """
-    inputs_dict = [
-        {
-            "x": 0,
-            "y": 0,
-            "z": 0,
-            "temp": 0
-        },
-        {
-            "x": 1,
-            "y": 2,
-            "z": -1,
-            "temp": -1
-        },
-        {
-            "x": 2,
-            "y": 1,
-            "z": 1,
-            "temp": 1
-        },
-        {
-            "x": 3,
-            "y": 3,
-            "z": 0,
-            "temp": 0
-        },
-        {
-            "x": 4,
-            "y": 2,
-            "z": 2,
-            "temp": 2
-        },
-        {
-            "x": 5,
-            "y": 4,
-            "z": 1,
-            "temp": 1
-        },
+
+    file = config.TESTS_DIR / 'integer_tests' / '3' / 'program.py'
+    positive = collect_positive_states_from_file(file)
+    positive[0]['temp'] = -1
+    negative = [
         {
             "x": 6,
-            "y": 3,
-            "z": 3,
-            "temp": 3
+            "temp": -21,
+            "z": 7,
+            "y": 86
         },
         {
-            "x": 7,
-            "y": 5,
-            "z": 2,
-            "temp": 2
+            "x": -87,
+            "temp": -31,
+            "z": 32,
+            "y": 1
         },
         {
-            "x": 8,
-            "y": 4,
-            "z": 4,
-            "temp": 4
+            "x": 89,
+            "temp": -93,
+            "z": -28,
+            "y": -75
         },
         {
-            "x": 9,
-            "y": 6,
-            "z": 3,
-            "temp": 3
+            "x": 76,
+            "temp": -15,
+            "z": -73,
+            "y": 64
+        },
+        {
+            "x": -52,
+            "temp": -96,
+            "z": 80,
+            "y": 12
         }
     ]
-
     start = datetime.now()
     print(start)
-    for p in enumerate_predicates(grammar_string, "LEXPR", inputs_dict):
-        print(p)
-    print(datetime.now)
+    print(find_loop_invariant(grammar_string, positive, negative))
     print(datetime.now() - start)
